07c4e56493be5a4607bcc02c75d551b7c019750c
jjieunyun <lesson0186@gmail.com>
2022-02-16 21:32:11 +0900

    1st

---
+++ 2장/js/text_color.js
@@ -0,0 +1,7 @@
+        //heading변수를 선언하고 h1태그를 저장한 꼴이 되었다.
+        let heading = document.querySelector("#heading");
+        //h1태그를 클릭을하면 글자색깔을 빨강색으로 설정하시오.
+        heading.onclick = function() {
+            heading.style.color = "red";
+        }
+            
